d83db78822a43fd795efc1400dd9579d6e090dc3
Edgar Dewsbery <edewsbery@gmail.com>
2019-09-10 09:58:15 +0100

    sequelize initial commit

---
+++ store/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config({path: '../.env'});
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": "helloSQL_development",
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "helloSQL_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "helloSQL_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
